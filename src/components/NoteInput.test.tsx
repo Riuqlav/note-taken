f57d1f5be306fa9a136134240d4b0b182eb3589c
Riuqlav <valquirsolza.vs@gmail.com>
2023-10-24 10:26:16 +0200

    Refactor note input test

--- src/components/NoteInput.test.tsx
+++ src/components/NoteInput.test.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import NoteInput from './NoteInput';
-import Note from '../models/Note';
 
-test('displays NoteInput', () => {
-  const note: Omit<Note, 'id'> = {title: '', content: '', createdAt: '', updatedAt: ''};
+describe('NoteInput', () => {
+  const mockSetNewNote = jest.fn();
   const mockAddNote = jest.fn();
-  const { getByText } = render(<NoteInput newNote={note} setNewNote={() => {}} addNote={mockAddNote} />);
-  
-  fireEvent.click(getByText('Add Note'));
-  expect(mockAddNote).toHaveBeenCalled();
+  const newNote = {
+    title: '',
+    content: '',
+    createdAt: '',
+    updatedAt: ''
+  };
+
+  test('renders and interacts', () => {
+    const { getByPlaceholderText, getByText } = render(
+      <NoteInput newNote={newNote} setNewNote={mockSetNewNote} addNote={mockAddNote} />
+    );
+
+    // Test input change for title
+    const titleInput = getByPlaceholderText('Title');
+    fireEvent.change(titleInput, { target: { value: 'New Title' } });
+    expect(mockSetNewNote).toHaveBeenCalledWith({ ...newNote, title: 'New Title' });
+
+    // Test input change for content
+    const contentInput = getByPlaceholderText('Content');
+    fireEvent.change(contentInput, { target: { value: 'New Content' } });
+    expect(mockSetNewNote).toHaveBeenCalledWith({ ...newNote, content: 'New Content' });
+
+    // Test addNote
+    fireEvent.click(getByText('Add Note'));
+    expect(mockAddNote).toHaveBeenCalled();
+  });
 });

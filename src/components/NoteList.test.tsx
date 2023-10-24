6847fa09e29fd6cd238be28bbe64cfbbf460b624
Riuqlav <valquirsolza.vs@gmail.com>
2023-10-24 10:26:33 +0200

    Create Note list test

---
+++ src/components/NoteList.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import NoteList from './NoteList';
+import Note from '../models/Note';
+
+describe('NoteList', () => {
+  const mockEdit = jest.fn();
+  const mockDelete = jest.fn();
+  const notes: Note[] = [
+    { id: '1', title: 'Note 1', content: 'Content 1', createdAt: 'date1', updatedAt: 'date1' },
+    { id: '2', title: 'Note 2', content: 'Content 2', createdAt: 'date2', updatedAt: 'date2' }
+  ];
+
+  test('renders notes', () => {
+    const { getAllByText } = render(<NoteList notes={notes} onEdit={mockEdit} onDelete={mockDelete} />);
+
+    // Check if titles are rendered
+    const renderedTitles = getAllByText(/Note \d/);
+    expect(renderedTitles).toHaveLength(2);
+    expect(renderedTitles[0]).toHaveTextContent('Note 1');
+    expect(renderedTitles[1]).toHaveTextContent('Note 2');
+
+    // If you have more details you want to assert, add them here
+  });
+});
